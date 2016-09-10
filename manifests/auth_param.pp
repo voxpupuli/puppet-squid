27701c2c9d5a7be4e0db862710104f3f030771a8
Jeff Frost <jeff.frost@procore.com>
2016-09-09 20:16:07 -0700

    Rearrange auth_params to come before acls
    The `auth_param` needs to come before `acl` in the configuration file;
    otherwise, squid will throw `ERROR: Invalid ACL`

--- manifests/auth_param.pp
+++ manifests/auth_param.pp
@@ -14,7 +14,7 @@ define squid::auth_param (
   concat::fragment{"squid_auth_param_${auth_param_name}":
     target  => $::squid::config,
     content => template('squid/squid.conf.auth_param.erb'),
-    order   => "40-${order}-${scheme}",
+    order   => "05-${order}-${scheme}",
   }
 
 }
