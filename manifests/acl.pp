8f8b7e32b5eb8497e0848727b99cb9a4beaaa780
Mattias Gees <mattias.gees@gmail.com>
2017-03-28 11:25:56 +0200

    Remove : from type so it can be used in order without errors

--- manifests/acl.pp
+++ manifests/acl.pp
@@ -6,10 +6,12 @@ define squid::acl (
   String $comment = "acl fragment for ${aclname}",
 ) {
 
+  $type_cleaned = regsubst($type,':','','G')
+
   concat::fragment{"squid_acl_${aclname}":
     target  => $::squid::config,
     content => template('squid/squid.conf.acl.erb'),
-    order   => "10-${order}-${type}",
+    order   => "10-${order}-${type_cleaned}",
   }
 
 }
