43bc802d0ca259ea1e903b4be95966432b1e9892
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2017-11-09 23:20:35 +0100

    Run acceptance tests on Debian 9
    We merge into one test because on Debian 9 the service won't start if
    there are no services defined.

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -1,34 +1,16 @@
 require 'spec_helper_acceptance'
 
 describe 'squid class' do
-  context 'default parameters' do
-    # Using puppet_apply as a helper
-    it 'works idempotently with no errors' do
-      pp = <<-EOS
-      class { 'squid':}
-      EOS
-
-      # Run it twice and test for idempotency
-      apply_manifest(pp, catch_failures: true)
-      apply_manifest(pp, catch_changes: true)
-    end
-
-    describe package("squid#{fact(:operatingsystem) == 'Debian' && fact(:operatingsystemmajrelease) == '8' ? '3' : ''}") do
-      it { is_expected.to be_installed }
-    end
-    describe service("squid#{fact(:operatingsystem) == 'Debian' && fact(:operatingsystemmajrelease) == '8' ? '3' : ''}") do
-      it { is_expected.to be_running }
-    end
-  end
-
   context 'configure http_access' do
+    squid_name = fact(:operatingsystem) == 'Debian' && fact(:operatingsystemmajrelease) == '8' ? 'squid3' : 'squid'
+
     it 'works idempotently with no errors' do
       pp = <<-EOS
       class { 'squid':}
       squid::http_port{'3128':}
       squid::acl{'our_networks':
         type    => src,
-	entries => ['all'],
+        entries => ['all'],
       }
       squid::http_access{'our_networks':
         action    => 'allow',
@@ -39,7 +21,17 @@ describe 'squid class' do
       apply_manifest(pp, catch_failures: true)
       apply_manifest(pp, catch_changes: true)
     end
-    describe file("/etc/squid#{fact(:operatingsystem) == 'Debian' && fact(:operatingsystemmajrelease) == '8' ? '3' : ''}/squid.conf") do
+
+    describe package(squid_name) do
+      it { is_expected.to be_installed }
+    end
+
+    describe service(squid_name) do
+      it { is_expected.to be_running }
+      it { is_expected.to be_enabled }
+    end
+
+    describe file("/etc/#{squid_name}/squid.conf") do
       it { is_expected.to be_file }
       it { is_expected.to contain(%r{^http_access allow our_networks\s*$}) }
       it { is_expected.to contain(%r{^http_port 3128\s*$}) }
