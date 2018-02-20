4cdfe85f30a6f717977142a37ec0d8d8ac43b012
Ralf Bosz <ralf@bosz.com>
2018-02-20 07:41:23 +0100

    This commit fixes the refresh_pattern template
    When using a regexp (when case_sensitive is set to false)
    the -i should be placed BEFORE the $name and not after
    Also fixed that no space was used between the max and
    options field, added rspec for options
    Removed default : which was used, this is not default,
    changed README so ^ftp: has it mentioned

--- spec/defines/refresh_pattern_spec.rb
+++ spec/defines/refresh_pattern_spec.rb
@@ -28,8 +28,27 @@ describe 'squid::refresh_pattern' do
         fname = 'squid_refresh_pattern_my_pattern'
         it { is_expected.to contain_concat_fragment(fname).with_target('/tmp/squid.conf') }
         it { is_expected.to contain_concat_fragment(fname).with_order('45-06') }
-        it { is_expected.to contain_concat_fragment(fname).with_content(%r{^refresh_pattern\s+my_pattern:\s+1440\s+20%\s+10080$}) }
+        it { is_expected.to contain_concat_fragment(fname).with_content(%r{^refresh_pattern\s+my_pattern\s+1440\s+20%\s+10080$}) }
       end # context 'when parameters are set'
+
+      context 'when parameters are set and options' do
+        let(:title) { 'my_pattern' }
+        let(:params) do
+          {
+            order:   '06',
+            max:     10_080,
+            min:     1440,
+            percent: 20,
+            options: 'override-expire ignore-no-cache',
+            comment: 'Refresh Patterns'
+          }
+        end
+
+        fname = 'squid_refresh_pattern_my_pattern'
+        it { is_expected.to contain_concat_fragment(fname).with_target('/tmp/squid.conf') }
+        it { is_expected.to contain_concat_fragment(fname).with_order('45-06') }
+        it { is_expected.to contain_concat_fragment(fname).with_content(%r{^refresh_pattern\s+my_pattern\s+1440\s+20%\s+10080\s+override-expire\s+ignore-no-cache$}) }
+      end
 
       context 'when parameters are set and case insensitive' do
         let(:title) { 'case_insensitive' }
@@ -47,7 +66,7 @@ describe 'squid::refresh_pattern' do
         fname = 'squid_refresh_pattern_case_insensitive'
         it { is_expected.to contain_concat_fragment(fname).with_target('/tmp/squid.conf') }
         it { is_expected.to contain_concat_fragment(fname).with_order('45-07') }
-        it { is_expected.to contain_concat_fragment(fname).with_content(%r{^refresh_pattern\s+case_insensitive:\s+-i\s+0\s+0%\s+0$}) }
+        it { is_expected.to contain_concat_fragment(fname).with_content(%r{^refresh_pattern\s+-i\s+case_insensitive\s+0\s+0%\s+0$}) }
       end # context 'when parameters are set and case insensitive'
     end
   end
