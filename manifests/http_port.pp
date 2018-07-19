37360d15cff20ceda9d0fcc1dd4e1a850e913a7c
Thomas Equeter <tequeter@users.noreply.github.com>
2018-07-19 17:39:13 +0200

    support listening on specific interfaces
    This allows "http_port host:port" configuration constructs, instead of just
    specifying the port and listening on all interfaces.
    
    Pre-1.0.0 versions used to allow "host:port" as port/title arg, because the
    input wasn't sufficiently checked.

--- manifests/http_port.pp
+++ manifests/http_port.pp
@@ -1,24 +1,46 @@
 define squid::http_port (
-  Variant[Pattern[/\d+/], Integer]
-          $port    = $title,
-  Boolean $ssl     = false,
-  String  $options = '',
-  String  $order   = '05',
+  Optional[Stdlib::Port] $port    = undef,
+  Optional[Stdlib::Host] $host    = undef,
+  Boolean                $ssl     = false,
+  String                 $options = '',
+  String                 $order   = '05',
 ) {
+  $_title = String($title)
+
+  if $port == undef {
+    if $_title =~ /^(?:.+:)?(\d+)$/ {
+      $_port = Integer($1)
+      if $_port !~ Stdlib::Port {
+        fail("invalid port determined from title: ${_port}")
+      }
+    } else {
+      fail("port couldn't be determined from title nor args")
+    }
+  } else {
+    $_port = $port
+  }
+
+  # Only grab the host from the title if no port arg given and the title is
+  # very likely to mean host:port. This should be backward-compatible with
+  # client code from before this feature was introduced.
+  if $port == undef and $host == undef and $_title =~ /^(.+):\d+$/ {
+    $_host = $1
+  } else {
+    $_host = $host # May be undef
+  }
 
   $protocol = $ssl ? {
     true    => 'https',
     default => 'http',
   }
 
-  concat::fragment{"squid_${protocol}_port_${port}":
+  concat::fragment{"squid_${protocol}_port_${_title}":
     target  => $squid::config,
     content => template('squid/squid.conf.port.erb'),
     order   => "30-${order}",
   }
 
   if $facts['selinux'] == true {
-    $_port = Integer($port)
     selinux::port{"selinux port squid_port_t ${_port}":
       ensure   => 'present',
       seltype  => 'squid_port_t',
@@ -28,4 +50,3 @@ define squid::http_port (
   }
 
 }
-
--- spec/defines/http_port_spec.rb
+++ spec/defines/http_port_spec.rb
@@ -12,25 +12,80 @@ describe 'squid::http_port' do
          }
         '
       end
-      let(:title) { '1000' }
 
       context 'when parameters are unset' do
+        let(:title) { '1000' }
+
         it { is_expected.to contain_concat_fragment('squid_http_port_1000').with_target('/tmp/squid.conf') }
         it { is_expected.to contain_concat_fragment('squid_http_port_1000').with_order('30-05') }
         it { is_expected.to contain_concat_fragment('squid_http_port_1000').with_content(%r{^http_port\s+1000\s*$}) }
       end
-      context 'when parameters are set' do
+      context 'when host:port title is set' do
+        let(:title) { '127.0.0.1:1500' }
+
+        it { is_expected.to contain_concat_fragment('squid_http_port_127.0.0.1:1500').with_content(%r{^http_port\s+127\.0\.0\.1:1500\s*$}) }
+      end
+      context 'with invalid port (non-numeric) in host:port title' do
+        let(:title) { 'my:test' }
+
+        it { is_expected.not_to compile }
+      end
+      context 'with invalid port (out of range) in host:port title' do
+        let(:title) { 'my:100000' }
+
+        it { is_expected.not_to compile }
+      end
+      context 'with "host: port" invalid title' do
+        let(:title) { 'host: 1600' }
+
+        it { is_expected.not_to compile }
+      end
+      context 'with host:port title and port arg' do
+        let(:title) { 'host:1650' }
         let(:params) do
           {
-            port: 2000,
-            options:  'special for 2000',
-            order: '08'
+            port: 1650
           }
         end
 
-        it { is_expected.to contain_concat_fragment('squid_http_port_2000').with_target('/tmp/squid.conf') }
-        it { is_expected.to contain_concat_fragment('squid_http_port_2000').with_order('30-08') }
-        it { is_expected.to contain_concat_fragment('squid_http_port_2000').with_content(%r{^http_port\s+2000\s+special for 2000$}) }
+        # Ignore the host part of the title if a port is specified
+        it { is_expected.to contain_concat_fragment('squid_http_port_host:1650').with_content(%r{^http_port\s+1650\s*$}) }
+      end
+      context 'when host and port parameters are set' do
+        let(:title) { 'test' }
+        let(:params) do
+          {
+            port: 1700,
+            host: '127.0.0.1'
+          }
+        end
+
+        it { is_expected.to contain_concat_fragment('squid_http_port_test').with_content(%r{^http_port\s+127\.0\.0\.1:1700\s*$}) }
+      end
+      context 'when parameters are set' do
+        let(:title) { 'my:test' } # Arguments shoud override title
+        let(:params) do
+          {
+            port:    2000,
+            options: 'special for 2000',
+            order:   '08'
+          }
+        end
+
+        it { is_expected.to contain_concat_fragment('squid_http_port_my:test').with_target('/tmp/squid.conf') }
+        it { is_expected.to contain_concat_fragment('squid_http_port_my:test').with_order('30-08') }
+        it { is_expected.to contain_concat_fragment('squid_http_port_my:test').with_content(%r{^http_port\s+2000\s+special for 2000$}) }
+      end
+      context 'with host overriding invalid title' do
+        let(:title) { 'my:test' }
+        let(:params) do
+          {
+            port:    2100,
+            host:    'host'
+          }
+        end
+
+        it { is_expected.to contain_concat_fragment('squid_http_port_my:test').with_content(%r{^http_port\s+host:2100\s*$}) }
       end
       context 'when ssl => true' do
         let(:title) { '3000' }
