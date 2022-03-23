e50723fd827cd141d693e46f28d31c060d71a0a2
Steve Traylen <steve.traylen@cern.ch>
2022-03-23 08:20:55 +0100

    pend a test to catch if notify in C8 is ever fixed.

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -1,7 +1,36 @@
 require 'spec_helper_acceptance'
 
 describe 'squid class' do
-  context 'configure http_access' do
+  context 'configure http_access with default service type' do
+    it 'works idempotently with no errors' do
+      pending('the default Type=notify in squid.service working again on CentOS 8') if fact('os.family') == 'RedHat' && fact('os.release.major') != '7'
+      pp = <<-EOS
+      # The default Type=notify is problematic in github CI.
+      if $facts['os']['family'] == 'RedHat' and $facts['os']['release']['major'] != '7' {
+        systemd::dropin_file{'simple.conf':
+          ensure => absent,
+          unit   => 'squid.service',
+          before => Service['squid'],
+        }
+      }
+      class { 'squid':}
+      squid::http_port{'3128':}
+      squid::acl{'our_networks':
+        type    => src,
+        entries => ['all'],
+      }
+      squid::http_access{'our_networks':
+        action    => 'allow',
+        comment   => 'Our networks hosts are allowed',
+      }
+      EOS
+      # Run it twice and test for idempotency
+      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_changes: true)
+    end
+  end
+
+  context 'configure http_access with simple service type' do
     it 'works idempotently with no errors' do
       pp = <<-EOS
       # The default Type=notify is problematic in github CI.
