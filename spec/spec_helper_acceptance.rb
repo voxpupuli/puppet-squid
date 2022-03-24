dd1042a407b6555f0c992ba7f7eff897cc5724e1
Steve Traylen <steve.traylen@cern.ch>
2022-03-24 09:03:58 +0100

    ruocop - Style/IfUnlessModifier - correction

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -3,9 +3,7 @@
 require 'voxpupuli/acceptance/spec_helper_acceptance'
 
 configure_beaker do |host|
-  if fact('os.family') == 'RedHat' && fact('os.release.major') != '7'
-    on host, puppet('module', 'install', 'puppet-systemd'), acceptable_exit_codes: [0, 1]
-  end
+  on host, puppet('module', 'install', 'puppet-systemd'), acceptable_exit_codes: [0, 1] if fact('os.family') == 'RedHat' && fact('os.release.major') != '7'
 end
 
 Dir['./spec/support/acceptance/**/*.rb'].sort.each { |f| require f }
