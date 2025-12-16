fdf4741c239cb25ade03236e74659da62b6097a0
artonix101 <artinab@posteo.de>
2025-12-16 22:27:21 +0100

    Add GB size unit support
    - Add GB to size unit parsing
    - Add tests for GB size units

--- spec/type_aliases/squid_size_spec.rb
+++ spec/type_aliases/squid_size_spec.rb
@@ -7,6 +7,7 @@ describe 'Squid::Size' do
   it { is_expected.to allow_value('1 MB') }
   it { is_expected.to allow_value('10 KB') }
   it { is_expected.to allow_value('9876543210 KB') }
+  it { is_expected.to allow_value('1 GB') }
   it { is_expected.not_to allow_value('-1 KB') }
   it { is_expected.not_to allow_value('1 kB') }
   it { is_expected.not_to allow_value('1 Kb') }
@@ -14,4 +15,6 @@ describe 'Squid::Size' do
   it { is_expected.not_to allow_value('1 KBB') }
   it { is_expected.not_to allow_value('a KBB') }
   it { is_expected.not_to allow_value('1KB') }
+  it { is_expected.not_to allow_value('1 gb') }
+  it { is_expected.not_to allow_value('1 Gb') }
 end
--- types/size.pp
+++ types/size.pp
@@ -1,3 +1,3 @@
 # @summary
 #  Custom type containing the numeral value and the unit of messurement (Kilo- or Megabyte)
-type Squid::Size = Pattern[/^\d+ [KM]B$/]
+type Squid::Size = Pattern[/^\d+ [KMG]B$/]
